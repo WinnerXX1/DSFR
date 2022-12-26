aedcd93a5cee4183cf51120efa0bb9052059ec9d
Winner XX <dengrechka312@gmail.com>
2022-12-26 06:47:11 +0300

    -m
    -m

---
+++ src/ENGINE.c
@@ -0,0 +1,11 @@
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <SDL_net.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
